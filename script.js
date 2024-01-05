66c3770cb6b071d3e9ecfc2282d05a520ce0a3d0
Chuck Hupert <chhupert91@gmail.com>
2024-01-05 15:12:29 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getCount();
+});
+
+const getCount = () => {
+    let count = 30;
+    fetch('#').then(response => {
+        return response.json();
+    }).then(response => {
+        console.log("API Called");
+        count = response.Item.count;
+        console.log(response)
+        console.log(count)
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
